6ee78b678649a51ea527155bc2332ab214e8a8c4
leapingfish <hart@leapingfish.io>
2015-10-19 18:08:20 -0400

    add notes

---
+++ 10-19-2015/validate/js/script.js
@@ -0,0 +1,64 @@
+console.log('hello world');
+
+$('form').validate({
+    submitHandler: function(form) {
+        console.log('SUBMIT THE FORM!');
+    }
+});
+
+$('[name="start_date"],[name="end_date"]').datepicker();
+
+$('[name="state"]').autocomplete({
+  source: [
+      "Alabama",
+      "Alaska",
+      "Arizona",
+      "Arkansas",
+      "California",
+      "Colorado",
+      "Connecticut",
+      "Delaware",
+      "Florida",
+      "Georgia",
+      "Hawaii",
+      "Idaho",
+      "Illinois",
+      "Indiana",
+      "Iowa",
+      "Kansas",
+      "Kentucky",
+      "Louisiana",
+      "Maine",
+      "Maryland",
+      "Massachusetts",
+      "Michigan",
+      "Minnesota",
+      "Mississippi",
+      "Missouri",
+      "Montana",
+      "Nebraska",
+      "Nevada",
+      "New Hampshire",
+      "New Jersey",
+      "New Mexico",
+      "New York",
+      "North Carolina",
+      "North Dakota",
+      "Ohio",
+      "Oklahoma",
+      "Oregon",
+      "Pennsylvania",
+      "Rhode Island",
+      "South Carolina",
+      "South Dakota",
+      "Tennessee",
+      "Texas",
+      "Utah",
+      "Vermont",
+      "Virginia",
+      "Washington",
+      "West Virginia",
+      "Wisconsin",
+      "Wyoming"
+    ]
+});
